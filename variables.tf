fd3962fcd7987c1e80bda4a4a28579c91ce0513b
sundar410 <43734758+sundar410@users.noreply.github.com>
2025-01-22 15:12:55 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "region" {
+  description = "AWS region for the VM"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID to use for the VM"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type (e.g., t2.micro)"
+  type        = string
+}
+
+variable "key_pair_name" {
+  description = "Name of the AWS key pair to use for SSH"
+  type        = string
+}
+
+variable "instance_name" {
+  description = "Name tag for the VM instance"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region for the VM"
+  type        = string
+}
