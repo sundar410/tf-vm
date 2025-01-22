95c50cf972b028f08d5e446dcdc4c7c4b0088169
sundar410 <43734758+sundar410@users.noreply.github.com>
2025-01-22 18:19:33 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -27,3 +27,8 @@ variable "vpc_id" {
   description = "The ID of the VPC where resources will be deployed"
   type        = string
 }
+
+variable "subnet_id" {
+  description = "The ID of the subnet where resources will be deployed"
+  type        = string
+}
