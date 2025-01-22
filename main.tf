becb8bab8d5c1c78edfd7543685f8b19d74e8ecc
sundar410 <43734758+sundar410@users.noreply.github.com>
2025-01-22 18:19:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 resource "aws_instance" "vm_instance" {
   ami           = var.ami_id
   instance_type = var.instance_type
-
+  subnet_id = var.subnet_id
 
   tags = {
     Name = var.instance_name
